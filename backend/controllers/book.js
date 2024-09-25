59e126470cc2b6b85a3191fa792a653ef4b7837d
Thibault Boucard Garda <thibault.boucardgarda@gmail.com>
2024-09-25 14:05:15 +0200

    Fin du projet, merge en cours

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -103,8 +103,9 @@ exports.rateOneBook = (req, res, next) => {
         return res.status(404).json({ message: "Livre inexistant" });
       }
 
-      const actualRate = book.ratings.find((rating) => rating.userId === userId);
-      if (actualRate) {
+      // Mieux nommer 
+      const changeRate = book.ratings.find((rating) => rating.userId === userId);
+      if (changeRate) {
         return res.status(400).json({ message: "Vous avez déjà donné une note à ce livre" });
       }
 
